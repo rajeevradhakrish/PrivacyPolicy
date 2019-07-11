a8b2ff95e0bb117f69709c7b2bbf55e547a1cfc8
Rajeev Radhakrishnan <rajeevradhakrish@gmail.com>
2019-07-11 09:40:53 +0200

    Initial Commit

---
+++ Privacy/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Privacy
+//
+//  Created by Rajeev Radhakrishnan on 11/07/2019.
+//  Copyright © 2019 RRIOSAPPS. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
